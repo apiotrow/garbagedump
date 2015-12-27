17662b597430631ea727d54c153fc897396696e9
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2015-12-26 18:26:57 -0800

    ramp game started

---
+++ garbagedump/Assets/MDESF/Scripts/MDESFrampcc.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MDESFrampcc : MonoBehaviour {
+	MDESFrampec enemyc;
+
+	Slider ramp;
+	public Text txt;
+	public float rampVal;
+	
+	void Start () {
+		ramp = GetComponent<Slider>();
+		enemyc = GameObject.Find("EnemyRamp").GetComponent<MDESFrampec>();
+		txt = transform.Find("Text").GetComponent<Text>();
+	}
+
+	void Update () {
+
+
+		if(enemyc.currMove != -1){
+			if(pressedRightButton()){
+				enemyc.resetMove();
+			}else if(pressedWrongButton()){
+				enemyc.rampFillColor = Color.red;
+			}
+		}
+
+	}
+
+	void rampUp(float speed){
+		ramp.value = Mathf.MoveTowards(ramp.value, 1f, Time.deltaTime * speed);
+	}
+
+	bool pressedRightButton(){
+		if(Input.GetKeyDown(enemyc.buttons[enemyc.currMove]))
+			return true;
+		return false;
+	}
+
+	bool pressedWrongButton(){
+		for(int i = 0; i < enemyc.buttons.Length; i++){
+			if(enemyc.currMove != i && Input.GetKeyDown(enemyc.buttons[i])){
+				return true;
+			}
+		}
+		return false;
+	}
+}
---
+++ garbagedump/Assets/MDESFrampec.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MDESFrampec : MonoBehaviour {
+	MDESFrampcc pc;
+
+	Slider ramp;
+	public Text txt;
+	public KeyCode[] buttons = new KeyCode[3]{KeyCode.Q, KeyCode.W, KeyCode.E};
+	string[] moves = new string[3] {"Q", "W", "E"};
+	float[] rampSpeeds = new float[3] {0.5f, 0.7f, 1f};
+	public int currMove = -1;
+	Image rampFill;
+	public Color rampFillColor = Color.green;
+	
+	void Start () {
+		ramp = GetComponent<Slider>();
+		pc = GameObject.Find("PlayerRamp").GetComponent<MDESFrampcc>();
+		txt = transform.Find("Text").GetComponent<Text>();
+		rampFill = transform.Find("Fill Area/Fill").GetComponent<Image>();
+	}
+	
+	void Update () {
+		rampFill.color = rampFillColor;
+
+		if(currMove == -1)
+			currMove = (int)Mathf.Floor(Random.Range(0f, (float)moves.Length));
+			
+		txt.text = moves[currMove];
+		rampUp(rampSpeeds[currMove]);
+
+		if(ramp.value == 1f){
+			resetMove();
+		}
+	}
+	
+	void rampUp(float speed){
+		ramp.value = Mathf.MoveTowards(ramp.value, 1f, Time.deltaTime * speed);
+	}
+
+	public void resetMove(){
+		currMove = -1;
+		ramp.value = 0f;
+		rampFillColor = Color.green;
+	}
+}
