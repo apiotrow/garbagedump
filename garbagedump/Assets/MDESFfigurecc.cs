b3f745ddf7ee8da9aaf3bb579b8d644a463b5a86
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2015-12-29 15:43:48 -0800

    second guy

---
+++ garbagedump/Assets/MDESFfigurecc.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MDESFfigurecc : MonoBehaviour {
+	Animator anim;
+	
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+
+	void Update () {
+		if(Input.GetKeyDown("1"))
+			anim.SetTrigger("punch");
+		else if(Input.GetKeyDown("2"))
+			anim.SetTrigger("kick");
+	}
+}
